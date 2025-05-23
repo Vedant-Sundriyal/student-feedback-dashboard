0e604496123c79ee0720c1ec4013e30d13e2ae56
Vedant-Sundriyal <sundriyalvedant51@gmail.com>
2025-05-23 00:16:29 -0400

    Fix build: downgrade heroicons and update import paths

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { AcademicCapIcon, UserIcon, ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline';
+import { AcademicCapIcon, UserIcon, ArrowRightOnRectangleIcon } from '@heroicons/react/outline';
 
 const Layout = ({ children }) => {
   const navigate = useNavigate();
